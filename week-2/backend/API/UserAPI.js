b491f04b0bc90842f6aa69f411d3e13d30b89de6
PuttaAnikha <puttaanikha25@gmail.com>
2026-05-14 14:00:40 +0530

    Updated week-2 programs

--- week-2/backend/API/UserAPI.js
+++ week-2/backend/API/UserAPI.js
@@ -5,41 +5,50 @@ export const userApp = exp.Router()
 let users=[];
 
 //create API(REST API- REpresentational state transfer)
+
  // route to handle GET req of client(http://localhost:3000/users)
- userApp.get('/users',(req,res)=>{
+   userApp.get('/users',(req,res)=>{
     /*send res to client
     res.json({message:"this res for get users req"})
- })*/
-res.json({message : "all users",payload:users})})
+   })*/
+   res.json({message : "all users",payload:users})})
+
  // route to handle POST req of client
-userApp.post('/users',(req,res)=>{
+   userApp.post('/users',(req,res)=>{
     /*send res to client
     res.json({message:"this res for post req"})
- })*/
-//get user from the client
-const newUser = req.body
-//push user into users
-users.push(newUser)
-//send res
-res.json({message:"user created"})
- })
+   })*/
+   
+ //get user from the client
+   const newUser = req.body
+
+ //push user into users
+   users.push(newUser)
+
+ //send res
+   res.json({message:"user created"})
+  })
+
  //route to handle PUT request of client
- userApp.put('/users',(req,res)=>{
+   userApp.put('/users',(req,res)=>{
     /*send res to client
     res.json({message:"this res for put req"})
- })*/
+   })*/
+
 //get modified user from client{}
-let modifiedUser = req.body;
+   let modifiedUser = req.body;
+
 //get index of existing user in users array
-let index = users.findIndex(userobj=>userobj.id===modifiedUser.id)
+  let index = users.findIndex(userobj=>userobj.id===modifiedUser.id)
   //user not found
   if(index===-1){
    return res.json({message : "user not found"})
   }
+
 //update user with index
-users.splice(index,1,modifiedUser)
+  users.splice(index,1,modifiedUser)
 //send res
-res.json({message:"user updated"})
+  res.json({message:"user updated"})
  })
  // route to handle Delete request of client
  userApp.delete('/users/:id',(req,res)=>{
